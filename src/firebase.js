6823ce93eb557b5f257e1999395ebcde9e8d78be
Sandeep Ashok <51964832+sandeepashok@users.noreply.github.com>
2023-11-27 09:22:01 +0530

    fix: firebase config updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore, collection, query, orderBy, serverTimestamp, addDoc, wher
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyCPvoKOfDnvkAWV1iCUkv7cxzbGnNsgeUE",
+    authDomain: "notes-app-b06d8.firebaseapp.com",
+    databaseURL: "https://notes-app-b06d8-default-rtdb.firebaseio.com",
+    projectId: "notes-app-b06d8",
+    storageBucket: "notes-app-b06d8.appspot.com",
+    messagingSenderId: "899928627443",
+    appId: "1:899928627443:web:7a0518696d6a3ef4bf7f86"
 };
 
 const app = initializeApp(firebaseConfig);
